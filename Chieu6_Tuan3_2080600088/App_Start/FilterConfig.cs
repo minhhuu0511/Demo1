e787bc9e59bbc4da733b262fdb85a91bbbe82b65
minhhuu0511 <114712061+minhhuu0511@users.noreply.github.com>
2023-03-02 09:39:52 +0700

    First Demo

---
+++ Chieu6_Tuan3_2080600088/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Chieu6_Tuan3_2080600088
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Chieu6_Tuan3_2080600088/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chieu6_Tuan3_2080600088.MvcApplication" Language="C#" %>
